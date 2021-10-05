19a9979ee641cc90aa30dc377a1d2d55178eee4c
yuqiyuqitan <tanyuqi527@gmail.com>
2021-10-05 04:12:34 +0000

    increase logging frequency

--- gp_train.py
+++ gp_train.py
@@ -129,15 +129,16 @@ def get_pipeline(train_dir, input_size, output_size, model = None, loss=None, vo
             },
             loss_inputs={0: pred, 1: gt, 2: mask},
             outputs={0: pred},
-            save_every = 400,
-            log_every = 10
+            save_every = 100,
+            log_every = 5
         )
         pipeline += gp.Snapshot(
             dataset_names = {raw: "raw", gt: "gt", pred: "pred"},
             output_dir='train_snapshot',
             output_filename = 'batch_{iteration}.zarr',
-            every = 100
+            every = 25
         )
+        
     else:    
         pipeline += gp.Snapshot(
             dataset_names = {raw: "raw_aug", gt: "gt_aug"},
